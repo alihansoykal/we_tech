8bbf2a0827db728493c033291cb2540754594129
Alihan Soykal <alihansoykal@gmail.com>
2021-10-15 20:43:18 +0200

    rfid fix

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,39 +2,67 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-
+List uids = [];
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final CollectionReference users =
+      FirebaseFirestore.instance.collection('users');
+
   @override
   void initState() {
     super.initState();
-    databaseReferenceTest
-        .child('3566')
-        .onValue.listen((event) {
+    init();
+  }
+
+  Future<void> init() async {
+    final CollectionReference users =
+        await FirebaseFirestore.instance.collection('users');
+    final databaseReferenceTest = FirebaseDatabase.instance.reference();
+
+    databaseReferenceTest.child('3566').onValue.listen((event) async {
       var snapshot = event.snapshot;
       String value = snapshot.value['RFID_TEMP'];
       print('Value is $value');
       databaseReferenceTest.once().then((DataSnapshot snapshot) {
-      print('Data : ${snapshot.value["3566"]["WEIGHT_ADDED"]}');
+        var data = snapshot.value["3566"]["WEIGHT_ADDED"];
+        print('Data : ${snapshot.value["3566"]["WEIGHT_ADDED"]}');
+        users
+            .where("rfId", isEqualTo: value)
+            .get()
+            .then((QuerySnapshot querySnapshot) {
+          querySnapshot.docs.forEach((element) {
+            uids.add(element["uid"]);
+            users.doc(element["uid"]).get().then((val) {
+              users.doc(element["uid"]).update({
+                "coins": val["coins"] + 2 * data,
+                "recycled": val["recycled"] + data
+              });
+            });
+          });
+        });
       });
-
-      //FirebaseFirestore.instance.collection("users").where("rfId",isEqualTo: value).get().then((QuerySnapshot querySnapshot ) => querySnapshot.docs.forEach((element) {print(element["name"]);}));
-      print("*************************");
-      // TODO: bileklik id sinin firestoreda kime ait olduğu kontrol edilip o kullanıcıya WEIGHT_ADDED datası ile ilgili işlemler yapılacak
-
+      uids.clear();
     });
+    await databaseReferenceTest.child('/3566/RFID_TEMP').set('');
   }
-  final databaseReferenceTest = FirebaseDatabase.instance.reference();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print(uids);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+        },
+      ),
       body: Container(color: Colors.blue),
     );
   }
